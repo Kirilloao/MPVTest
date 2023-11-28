deec57bfd94e15b3792d9a3cf1f8ad5ff1e6820d
Kirilloao <kirilltaraturin747@gmail.com>
2023-11-28 11:46:41 +0100

    Initial Commit

---
+++ MPVTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MPVTest
+//
+//  Created by Kirill Taraturin on 28.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
